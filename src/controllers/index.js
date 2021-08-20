73fa68c147c18d15db95e6209b58c1ba5e3b0820
marinawangmw <marinawangmw@gmail.com>
2021-08-20 16:19:31 -0300

    Add movie controller axios configuration

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,2 +1,3 @@
 export { HttpClient } from '@/controllers/HttpClient';
 export { UserController } from '@/controllers/UserController';
+export { MovieController } from '@/controllers/MovieController';
